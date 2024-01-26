af5068d089ee4fae87900a77e6d211367f55c698
Jannik Oeschger <oeschgerjannik@gmail.com>
2024-01-26 17:28:40 +0100

    🐛 Made gradlew executable

---
+++ src/main/java/de/jahshaka/smp/Smp.java
@@ -0,0 +1,12 @@
+package de.jahshaka.smp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Smp extends JavaPlugin {
+
+    public void onEnable() {
+        Bukkit.getLogger().info("SMP-Plugin has been enabled!");
+    }
+
+}
